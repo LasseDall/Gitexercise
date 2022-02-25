8cacc2fb4f19219698e20ec29c3ee9970ae13b61
LasseDall <lass794rrstud.kea.dk>
2022-02-25 13:12:21 +0100

    added greet

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,15 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        Scanner sc = new Scanner(System.in);
         System.out.println("It works!");
+        System.out.println("Indtast navn:");
+        String navn = sc.nextLine();
+        System.out.println("Hej " + navn);
     }
 }
